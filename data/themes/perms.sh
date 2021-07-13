ed59a64b2a3cfb6dda4cdf62aef54fa6ed2a6174
jf-simon <simon@t-tischer.de>
2021-07-13 20:05:03 +0200

    Update perms.sh

--- data/themes/perms.sh
+++ data/themes/perms.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-chmod a+r "${DESTDIR}/${MESON_INSTALL_PREFIX}/share/enotes/themes/enotes.edj"
-chmod a+r "${DESTDIR}/${MESON_INSTALL_PREFIX}/share/enotes/themes/entry_theme.edj"
+chmod a+r "${DESTDIR}/${MESON_INSTALL_PREFIX}/share/enotes/themes/e-fprint-gui.edj"
+
